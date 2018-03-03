ff6b3cde4d4871177ff2f8a2f97d864a5b2e377e
aniket1018 <35362183+aniket1018@users.noreply.github.com>
2018-03-03 23:30:19 +0530

    second_commit
    defined some more functions consumer and product.

--- commit.c
+++ commit.c
@@ -1,25 +1,27 @@
-#include <stdio.h>
-#include <time.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-#include <pthread.h>
-#include <semaphore.h>
 
-sem_t buferMut,empty,full;
-int *array,arraypos=-1,productnum,consumernum,arraylen;
-pthread_t *ProduCer,*ConsuMer;
-
-void Cons(int p,pthread_t self){
+int Prod(pthread_t self){
 	int i = 0;
-	while( !pthread_equal(*(ConsuMer + i),self) && i < consumernum ){
+	int a = 1 + rand()%40;
+	while( !pthread_equal(*(ProduCer + i),self) && i < productnum ){
 	i++;
 	}
-
-	printf("\nBlock:");
-	for(i = 0; i <= arraypos; ++i)
-	printf("%d ",*(array + i));
-	printf("\nThe Consumer no. %d consumed %d \nCurrent block length: %d\n",i + 1, p, arraypos);
-	printf("---------------------------------------------------------------------------------------");
-
+	printf("\nThe producer no. %d produced %d", i + 1, a);
+	return a;
 }
+
+void* Consumer(){
+	int w;
+	while(1){
+	sem_wait(&full);
+	sem_wait(&buferMut);
+	w = *(array + arraypos);
+	Cons(w, pthread_self());
+	--arraypos;
+	sem_post(&buferMut);
+	sem_post(&empty);
+	sleep(1 + rand()%3);
+	}
+
+	return NULL;
+}
+
