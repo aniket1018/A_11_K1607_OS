8a1ab179c2f6ffa139628e9e7784a508662030ff
aniket1018 <35362183+aniket1018@users.noreply.github.com>
2018-03-12 19:31:59 +0530

    third_commit
    Defined producer function.

--- commit.c
+++ commit.c
@@ -1,3 +1,28 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <pthread.h>
+#include <semaphore.h>
+
+sem_t buferMut,empty,full;
+int *array,arraypos=-1,productnum,consumernum,arraylen;
+pthread_t *ProduCer,*ConsuMer;
+
+void Cons(int p,pthread_t self){
+	int i = 0;
+	while( !pthread_equal(*(ConsuMer + i),self) && i < consumernum ){
+	i++;
+	}
+
+	printf("\nBlock:");
+	for(i = 0; i <= arraypos; ++i)
+	printf("%d ",*(array + i));
+	printf("\nThe Consumer no. %d consumed %d \nCurrent block length: %d\n",i + 1, p, arraypos);
+	printf("---------------------------------------------------------------------------------------");
+
+}
 
 int Prod(pthread_t self){
 	int i = 0;
@@ -22,6 +47,20 @@ void* Consumer(){
 	sleep(1 + rand()%3);
 	}
 
+	return NULL;
+}
+
+void* Producer(){
+	while(1){
+	int v = Prod(pthread_self());
+	sem_wait(&empty);
+	sem_wait(&buferMut);
+	++arraypos; //critical section
+	*(array + arraypos) = v;
+	sem_post(&buferMut);
+	sem_post(&full);
+	sleep(1 + rand()%3);
+	}
 	return NULL;
 }
 
